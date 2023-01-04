de0176ba6210e2b260f8ce9fc6cf536d9535a444
Brainstorm <khameleonstorm@gmail.com>
2023-01-04 15:45:39 +0100

    first deploy

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,7 +2,7 @@ import HomeHero from "../../components/HomeHero/HomeHero"
 import ContactForm from "../../components/ContactForm/ContactForm"
 import Footer from "../../components/Footer/Footer"
 import { contact } from "../../utils/heroes"
-import Navbar from "../../components/NavBar/NavBar"
+import Navbar from "../../components/Navbar/Navbar"
 
 export default function index() {
   return (
